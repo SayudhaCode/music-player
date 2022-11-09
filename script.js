456d8136919d2de13350134eddab4b92dcf444db
sayudha <mazaya.studio@outlook.com>
2022-11-09 17:12:49 +0700

    update event progressbar

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -82,6 +84,16 @@ function nextSong() {
 // on load
 loadSong(songs[songIndex]);
 
+// update progress bar
+function updateProgressBar(e) {
+	if (isPlaying) {
+		const {duration, currentTime} = e.target;
+		const progressPercent = (currentTime / duration) * 100;
+		progress.style.width = `${progressPercent}%`;
+	}
+}
+
 // event next prev
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
